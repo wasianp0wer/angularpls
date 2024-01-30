74739296ae86a275c011ad9241b1df85184e97e9
Madelyn Harrington <madelyn.j.harrington@gmail.com>
2024-01-30 10:54:39 -0500

    removed more unnecessary comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { CompletionItem, CompletionItemKind, CompletionItemProvider, InlineCompletionItemProvider, Position, TextDocument } from 'vscode';
 import * as fs from 'fs';
